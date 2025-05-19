158700f4316a145d7976786b0610e27d30d647b1
Linto110 <lintoemmanuel8@gmail.com>
2025-05-19 15:19:38 +0530

    Add files via upload

--- wcount.sh
+++ wcount.sh
@@ -1,16 +1,25 @@
-if [ $# -ne 1 ];then
+if [ $# -gt 1 ];then
 echo "syntax is $0 <filename>"
-exit 0
+exit 1
 fi
+flag=0
+if [ $# -eq 1 ]
+then
 tt=`tty`
 exec<$1
+flag=1
+fi
 while read line
 do
 nol=`expr $nol + 1`
-set $line > /dev/null
+set $line 
 nowd=`expr $nowd + $#`
 done
 echo "No of words :$nowd and no of lines:$nol"
+if [ $flag -eq 1 ]
+then
 exec<$tt
+fi
 exit 0
+fi
 
